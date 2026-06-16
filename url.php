b9afaf4b2e2df48b44c5b65285fde92ed7904611
kong0107 <kong0107@gmail.com>
2026-06-16 22:51:59 +0800

    fix parse_dataurl(); add parse_mime_type()

--- url.php
+++ url.php
@@ -46,21 +46,88 @@ function jwt_decode($token, $associative = false) {
 
 
 /**
- * Parse a Data URL and return its components
- * @todo support mime type with appending options
- * @todo support input not encoded in base64
- * @see https://developer.mozilla.org/en-US/docs/Web/URI/Reference/Schemes/data
- * @param string $url
- * @throws ValueError
- * @return array
+ * Encodes data into JWT
+ * @todo does not work yet!!!
  */
-function parse_dataurl($url) {
-	if (preg_match('/^data:(\w+)\/([\w\.\-]+);base64,/', $url, $matches)) {
-		return array(
-			'type' => $matchs[0],
-			'subtype' => $matches[1],
-			'base64' => substr($url, strlen($matches[0]))
+function jwt_encode(
+	string $algo,
+	mixed $payload,
+	string $secret
+) : string {
+	$str = base64url_encode('{"alg":"' . $algo . '","typ":"JWT"}')
+		. '.'
+		. base64url_encode(is_string($payload) ? $payload : json_encode($payload, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+	;
+	openssl_sign($str, $signature, $secret, $algo); ///< todo
+	return "$str.$signature";
+}
+
+
+/**
+ *
+ */
+function parse_mime_type(
+	string $mime_type,
+	bool $associative = false
+) : array|false {
+	$pattern = implode('', array(
+		'#^([^\s/;=,]+)',
+		'/([^\s/;=,]+?)',
+		'(?:\+([^\s/;=,+]+))?',
+		'(?:\s*;(.+))?$#'
+	));
+	if (!preg_match($pattern, $mime_type, $matches)) return false;
+	list (, $type, $subtype, $suffix, $para_list) = $matches;
+
+	$parameters = null;
+	if ($para_list) {
+		preg_match_all(
+			'/([^\s\/;=,]+)\s*=\s*(?:"((?:[^"\\]|\\.)*)"|([^\s\/;=,]+))/',
+			$para_list, $matches, PREG_SET_ORDER
 		);
+		$parameters = array();
+		foreach ($matches as $match) {
+			$key = strtolower($match[1]);
+			if (str_ends_with($key, '*')
+				&& preg_match("/^([^']+)''(.+)$/", $match[3], $v_match)
+			) {
+				/// for "filename*=UTF-8''%E6%B8%AC%E8%A9%A6.txt"
+				$key = substr($key, 0, -1);
+				$value = rawurldecode($v_match[2]);
+				if (strcasecmp($v_match[1], 'UTF-8'))
+					$value = mb_convert_encoding($value, 'UTF-8', $v_match[1]);
+			}
+			else $value = $match[3] ?: stripcslashes($match[2] ?? '');
+			$parameters[$key] = $value;
+		}
+	}
+
+	$result = array(
+		'type' => $type,
+		'subtype' => $subtype,
+		'facets' => explode('.', $subtype),
+		'suffix' => $suffix,
+		'parameters' => $parameters
+	);
+	return $associative ? $result : (object) $result;
+}
+
+
+/**
+ * Parse a Data URL and return its components
+ * @see https://developer.mozilla.org/en-US/docs/Web/URI/Reference/Schemes/data
+ * @throws ValueError
+ */
+function parse_dataurl(
+	string $url,
+	bool $associative = false
+) : array|object {
+	if (preg_match('/^data:([^;=,]+)(;base64)?,/', $url, $matches)) {
+		$raw = substr($url, strlen($matches[0]));
+		$info = parse_mime_type($matches[1]);
+		$info['mime'] = $matches[1];
+		$info['data'] = $matches[2] ? base64_decode($raw) : rawurldecode($raw);
+		return $associative ? $info : (object) $info;
 	}
 	else throw new ValueError('not a data URL');
 }
